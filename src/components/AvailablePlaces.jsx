25b69bcc5050986005ca1281d442cbc7cc064d4b
Xuan Thanh <lxthanh235@gmail.com>
2023-11-11 03:03:31 +0700

    FETCH DATA INSTALL BODY_PARSER

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,12 +1,28 @@
-import Places from './Places.jsx';
+import { useEffect, useState } from "react";
+import Places from "./Places.jsx";
+
+const places = localStorage.getItem("places");
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  return (
-    <Places
-      title="Available Places"
-      places={[]}
-      fallbackText="No places available."
-      onSelectPlace={onSelectPlace}
-    />
-  );
+ const [availablePlaces, setAvailablePlaces] = useState([]);
+
+ //useEffect to prevent infinite loop --> will executed once
+ useEffect(() => {
+  fetch("http://localhost:3000/places")
+   .then((res) => {
+    return res.json();
+   })
+   .then((resData) => {
+    setAvailablePlaces(resData.places);
+   });
+ }, []);
+
+ return (
+  <Places
+   title="Available Places"
+   places={availablePlaces}
+   fallbackText="No places available."
+   onSelectPlace={onSelectPlace}
+  />
+ );
 }
